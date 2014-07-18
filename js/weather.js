8bacfe8d2feee0cd2f1968a88a6c94c6683b9be0
Prashant Palikhe <prashant@osage.nl>
2014-07-18 15:24:03 +0200

    What has happened? A LOT

--- js/weather.js
+++ js/weather.js
@@ -1,70 +1,118 @@
-angular.module('myapp', ['ngAutocomplete'])
-    .controller('WeatherController', function ($scope, $http) {
-        var WEATHER_API  = 'http://api.openweathermap.org/data/2.5/weather?q=';
-        var FORECAST_API = 'http://api.openweathermap.org/data/2.5/forecast/daily?count=7&q=';
+var app = angular.module('myapp', ['ngAutocomplete']);
 
-        $scope.data = {
-            units: "metric",
-            city: "",
-            options: {
-                country: 'nl',
-                types: '(cities)'
-            },
-            details: ""
-        };
+/**
+ * CONTROLLERS
+ */
+app.controller('WeatherController', function ($scope, $http) {
+    var WEATHER_API  = 'http://api.openweathermap.org/data/2.5/weather?q=';
+    var FORECAST_API = 'http://api.openweathermap.org/data/2.5/forecast/daily?count=7&q=';
 
-        $scope.fetchData = function () {
-            var city = $scope.data.city.substring(0, $scope.data.city.indexOf(','));
+    $scope.units   = "metric";
+    $scope.city    = "";
+    $scope.options = {types: '(cities)'};
+    $scope.details = "";
+    $scope.data    = {};
 
-            if (!city) {
-                return;
-            }
+    $scope.resetData = function () {
+        $scope.data.weather = $scope.data.forecasts = $scope.data.error = undefined;
+        return $scope;
+    };
 
-            $scope.data.weather = $scope.data.forecasts = $scope.data.error = undefined;
+    $scope.fetchData = function () {
+        var comma = $scope.city.indexOf(',');
 
-            $scope.fetchWeather(city).fetchForecast(city);
-        };
+        if (comma > 0) {
+            var city = $scope.city.substring(0, $scope.city.indexOf(','));
+        }  else {
+            var city = $scope.city;
+        }
 
-        $scope.fetchWeather = function (city) {
-            var url = WEATHER_API + city + '&units=' + $scope.data.units;
+        if (!city) {
+            return;
+        }
 
-            $http
-                .get(url)
-                .success(function (data) {
-                    if (data.cod === "404") {
-                        $scope.data.error = "No data found for " + $scope.data.city;
-                    } else {
-                        $scope.data.weather = data.main;
-                    }
-                });
+        $scope.resetData().fetchWeather(city).fetchForecast(city);
+    };
 
-            return $scope;
-        };
+    $scope.fetchWeather = function (city) {
+        $scope.showloader = true;
+        var url = WEATHER_API + city + '&units=' + $scope.units;
 
-        $scope.fetchForecast = function (city) {
-            var url = FORECAST_API + city + '&units=' + $scope.data.units;
+        $http
+            .get(url)
+            .success(function (data) {
+                if (data.cod === "404") {
+                    $scope.data.error = "No data found for " + $scope.city;
+                } else {
+                    $scope.data.period = (data.dt > data.sys.sunset || data.dt < data.sys.sunrise) ? "night" : "day";
+                    $scope.data.weather = data.main;
+                    $scope.data.weather.icon = data.weather[0].icon;
+                }
+            });
 
-            $http
-                .get(url)
-                .success(function (data) {
-                    if (data.cnt) {
-                        $scope.data.forecasts = data.list;
-                    }
-                });
+        return $scope;
+    };
 
-            return $scope;
-        };
+    $scope.fetchForecast = function (city) {
+        var url = FORECAST_API + city + '&units=' + $scope.units;
 
-        $scope.toC = function () {
-            $scope.data.units = "metric";
-            $scope.fetchData();
-        };
+        $http
+            .get(url)
+            .success(function (data) {
+                $scope.showloader = false;
+                if (data.cnt) {
+                    $scope.data.forecasts = data.list;
+                }
+            });
 
-        $scope.toF = function () {
-            $scope.data.units = "imperial";
-            $scope.fetchData();
-        };
-    })
-    .config(function($httpProvider){
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    });
+        return $scope;
+    };
+
+    $scope.toC = function () {
+        $scope.units = "metric";
+        $scope.fetchData();
+    };
+
+    $scope.toF = function () {
+        $scope.units = "imperial";
+        $scope.fetchData();
+    };
+});
+
+/**
+ * CONFIGS
+ */
+app.config(function($httpProvider){
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
+
+
+/**
+ * FILTERS
+ */
+app.filter('beautifulicon', function () {
+    var iconMapping = {
+        "01d" : "wi-day-sunny" ,
+        "02d" : "wi-day-cloudy",
+        "03d" : "wi-cloudy",
+        "04d" : "wi-cloudy",
+        "09d" : "wi-showers",
+        "10d" : "wi-day-rain",
+        "11d" : "wi-thunderstorm",
+        "13d" : "wi-snow",
+        "50d" : "wi-fog",
+        "01n" : "wi-night-clear",
+        "02n" : "wi-night-cloudy",
+        "03n" : "wi-cloudy",
+        "04n" : "wi-cloudy",
+        "09n" : "wi-showers",
+        "10n" : "wi-night-rain",
+        "11n" : "wi-thunderstorm",
+        "13n" : "wi-snow",
+        "50n" : "wi-fog"
+    };
+
+    return function (input) {
+        return iconMapping[input];
+    };
+});
