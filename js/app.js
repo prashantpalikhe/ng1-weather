fd73755ad1049a73d347729128476d6d51aa941a
Prashant Palikhe <prashpal09@gmail.com>
2014-07-20 22:47:01 +0200

    Filter for climacons

--- js/app.js
+++ js/app.js
@@ -20,33 +20,3 @@ app.config(["$routeProvider", function ($routeProvider) {
             redirectTo: '/'
         });
     }]);
-
-
-app.filter('climacon', function () {
-        'use strict';
-
-        var iconMapping = {
-            "01d" : "sun" ,
-            "02d" : "cloudSun",
-            "03d" : "cloud",
-            "04d" : "cloud",
-            "09d" : "cloudDrizzleSun",
-            "10d" : "cloudRainSun",
-            "11d" : "cloudLightning",
-            "13d" : "cloudSnow",
-            "50d" : "cloudFog",
-            "01n" : "moon",
-            "02n" : "cloudMoon",
-            "03n" : "cloud",
-            "04n" : "cloud",
-            "09n" : "cloudDrizzleMoon",
-            "10n" : "cloudRainMoon",
-            "11n" : "cloudLightning",
-            "13n" : "cloudSnow",
-            "50n" : "cloudFog"
-        };
-
-        return function (input) {
-            return iconMapping[input];
-        };
-    });
