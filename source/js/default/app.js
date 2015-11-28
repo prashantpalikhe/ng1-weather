f4c141b9b03be173e4003230935bb3635adb7029
Prashant Palikhe <prashant@osage.nl>
2015-11-28 19:14:45 +0100

    Code upgrade

--- source/js/default/app.js
+++ source/js/default/app.js
@@ -6,17 +6,15 @@
         .config(["$routeProvider", function ($routeProvider) {
             'use strict';
 
-            $routeProvider.when('/', {
-                controller: 'WeatherController',
-                templateUrl: 'module/weather.html'
-            }).when('/:city', {
-                controller: 'WeatherController',
-                templateUrl: 'module/weather.html'
-            }).otherwise({
-                redirectTo: '/'
-            });
+            $routeProvider
+                .when('/:city?', {
+                    controller: 'WeatherController',
+                    templateUrl: 'module/weather.html'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
         }])
         .constant('OWM_API_URL', 'http://api.openweathermap.org/data/2.5')
-        .constant('OWM_API_KEY', '909876c41f18d47b83800fdcad715819')
-
+        .constant('OWM_API_KEY', '909876c41f18d47b83800fdcad715819');
 })();
